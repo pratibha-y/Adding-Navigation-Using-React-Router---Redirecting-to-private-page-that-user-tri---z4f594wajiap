38f5107f236a3cc5c6b616bc70e89ba2cb10061a
pratibha-y <76511037+pratibha-y@users.noreply.github.com>
2023-03-19 13:18:00 +0530

    Create NoMatch.js

---
+++ src/components/NoMatch.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import LocationDisplay from './LocationDisplay';
+
+export default class NoMatch extends Component {
+	render() {
+		return (
+			<>
+				<div>No match</div>
+			</>
+		);
+	}
+}
