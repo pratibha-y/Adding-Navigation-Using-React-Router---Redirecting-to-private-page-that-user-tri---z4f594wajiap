9fd691724b899783249e780f6aa6870b6b9a0126
pratibha-y <76511037+pratibha-y@users.noreply.github.com>
2023-03-19 23:34:37 +0530

    Update LocationDisplay.js

--- src/components/LocationDisplay.js
+++ src/components/LocationDisplay.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 
-function LocationDisplay() {
+export default function LocationDisplay() {
 	const { pathname } = useLocation();
 	return <div data-testid="location-display">{pathname}</div>;
 }
-
-export default LocationDisplay;
