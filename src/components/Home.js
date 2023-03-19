ae65fcb84ed8e4d853eece78479cb180ce8817a4
pratibha-y <76511037+pratibha-y@users.noreply.github.com>
2023-03-19 13:15:43 +0530

    Create Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import LocationDisplay from './LocationDisplay';
+
+export default class Home extends Component {
+	render() {
+		return (
+			<>
+				<div>You are home</div>
+			</>
+		);
+	}
+}
