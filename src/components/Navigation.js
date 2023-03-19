776d15c588648f536c1dff6abe2bb9b3cd3c2262
pratibha-y <76511037+pratibha-y@users.noreply.github.com>
2023-03-19 13:16:55 +0530

    Create Navigation.js

---
+++ src/components/Navigation.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Navigation extends Component {
+	render() {
+		return (
+			<nav>
+				<ul>
+					<li>
+						<Link to={'/'}>Home</Link>
+					</li>
+					<li>
+						<Link to={'/about'}>About</Link>
+					</li>
+				</ul>
+			</nav>
+		);
+	}
+}
+
+export default Navigation;
