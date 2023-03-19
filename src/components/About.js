582da57755ff391a95aaba5ba1e3c901ccb6c9de
pratibha-y <76511037+pratibha-y@users.noreply.github.com>
2023-03-19 13:14:36 +0530

    Create About.js

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import LocationDisplay from './LocationDisplay';
+
+export default class About extends Component {
+	render() {
+		return (
+			<>
+				<div>You are on the about page</div>
+			</>
+		);
+	}
+}
